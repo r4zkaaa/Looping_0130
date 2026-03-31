7f60e0ac93c8b67809d9bba1abcdb69d5fdca1c8
r4zkaaa <rzkiiahmd11@gmail.com>
2026-03-31 20:59:28 +0800

    Do while bilangan acak

--- looping.cpp
+++ looping.cpp
@@ -26,4 +26,11 @@ while(x<=5){
 
 cout << "Bilangan acak while yang terakhir =" << x << endl;
 cout << endl;
+//Perulangan dengan DO. . . WHILE
+cout<<"PERULANGANN DO ... WHILE" << endl;
+x = 1 + rand( )%10;
+do{
+    cout << "Bilangan acak = "<< x << endl;
+    x = rand()%10;
+}while(x <= 5);
 
